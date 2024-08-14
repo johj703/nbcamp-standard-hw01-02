a1303540eab689bda4f14e3ebdef0767f9df4d8b
Jo <johj703@nate.com>
2024-08-14 16:44:00 +0900

    App 파일에 Header, Content, Footer 컴포넌트 import 후 return 값에 추가

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,15 @@
+import Header from "./components/Header";
+import Content from "./components/Content";
+import Footer from "./components/Footer";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Header />
+      <Content />
+      <Footer />
+    </>
+  );
 }
 
 export default App;
